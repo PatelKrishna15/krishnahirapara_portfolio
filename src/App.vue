b8bfff328fe92d523d9b3391b9781fa225fbe741
PatelKrishna15 <krishahirapara666@gmail.com>
2024-01-24 18:22:33 +0530

    changes background color

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer.vue";
 <template>
   <header >
    
-    <nav class="bg-gray-500 ">
+    <nav class="bg-white-500 ">
       <div class="max-w-screen-xl  py-3 mx-auto flex items-center">
         <img src="@/assets/hk_logo.jpeg" alt="Logo" class="h-12 w-auto px-1" />
         <ul
