2d1535a7d06a30144fab6f8006bce2b208afdf60
PatelKrishna15 <krishahirapara666@gmail.com>
2024-01-30 10:11:04 +0530

    add footer content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="bg-gray-200 mt-20 py-3">
       <div class="container mx-auto text-center">
-        <span class="text-black">Place sticky footer content here.</span>
+        <span class="text-black">All Rights Reserve &copy; Created By Krishna Hirapara</span>
       </div>
     </footer>
 </template>
